6c05eb1b552a904e071b83609709356126fe7749
CartoonX <36625874+CartoonX@users.noreply.github.com>
2018-02-19 12:47:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,4 +12,4 @@ client.on('message', message => {
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.NDE1MTA2ODY0NzEyMzg0NTEy.DWxGDw.CZuIMDxx4IVyrWdbi_2TVfiLUTI);
+client.login(process.env.BOT_TOKEN);
